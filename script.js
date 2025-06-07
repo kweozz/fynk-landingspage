722fbf1f82976bbc382f4f21bc37d166d97ba841
Ilian Bosserz <iil.bossserez@gmail.com>
2025-06-07 23:08:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.nav-menu');
+const navOverlay = document.querySelector('.nav-overlay');
+
+burger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  navOverlay.classList.toggle('active');
+});
+
+// Optional: close menu when clicking the overlay
+navOverlay.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  navOverlay.classList.remove('active');
+});
